e191f6522fcc9ad866dbea3e69f555356a524b3c
Emilio Silva Schlenker <e18r@disroot.org>
2020-10-08 20:41:21 -0500

    inicialización de 3box

--- src/MarkethPlace.js
+++ src/MarkethPlace.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState, } from 'react';
+import Box from '3box';
+import Web3 from 'web3';
 
 const MarkethPlace = () => {
+
+  const [ web3, setWeb3, ] = useState();
+  const [ address, setAddress, ] = useState();
+  const [ box, setBox, ] = useState();
+
+  useEffect(() => {
+    setWeb3(new Web3(Web3.givenProvider));
+  }, [ setWeb3, ]);
+
+  useEffect(() => {
+    if (!web3) return;
+    const getAccounts = async () => {
+      const accounts = await web3.eth.requestAccounts();
+      setAddress(accounts[0]);
+    }
+    getAccounts();
+  }, [ web3, setAddress, ]);
+
+  useEffect(() => {
+    setBox(Box.openBox(address, Web3.givenProvider));
+  }, [ setBox, address, ]);
+
   return (
     <div>hola, mundo!</div>
   );
