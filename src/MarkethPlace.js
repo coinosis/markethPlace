64d30cd63e90d4489f904f965a5c464d819ee38d
Emilio Silva Schlenker <e18r@disroot.org>
2020-10-15 20:05:16 -0500

    botón de conectar con Metamask

--- src/MarkethPlace.js
+++ src/MarkethPlace.js
@@ -1,32 +1,49 @@
-import React, { useEffect, useState, } from 'react';
+import React, { useCallback, useEffect, useState, } from 'react';
 import Box from '3box';
 import Web3 from 'web3';
+import { MetaMaskButton, Loader, } from 'rimble-ui';
 
 const MarkethPlace = () => {
 
   const [ web3, setWeb3, ] = useState();
   const [ address, setAddress, ] = useState();
   const [ box, setBox, ] = useState();
+  const [ loading, setLoading, ] = useState();
 
   useEffect(() => {
     setWeb3(new Web3(Web3.givenProvider));
   }, [ setWeb3, ]);
 
-  useEffect(() => {
-    if (!web3) return;
-    const getAccounts = async () => {
-      const accounts = await web3.eth.requestAccounts();
-      setAddress(accounts[0]);
-    }
-    getAccounts();
-  }, [ web3, setAddress, ]);
+  const connect = useCallback(async () => {
+    setLoading(true);
+    const accounts = await web3.eth.requestAccounts();
+    setAddress(accounts[0]);
+  }, [ web3, setAddress, setLoading, ]);
 
   useEffect(() => {
-    setBox(Box.openBox(address, Web3.givenProvider));
-  }, [ setBox, address, ]);
+    if (!address) return;
+    const getBox = async () => {
+      setBox(await Box.openBox(address, Web3.givenProvider));
+      setLoading(false);
+    }
+    getBox();
+  }, [ setBox, address, setLoading, ]);
 
   return (
-    <div>hola, mundo!</div>
+    <div
+      css={`
+        display: flex;
+      `}
+    >
+      { !address && (
+        <MetaMaskButton
+          onClick={ connect }
+        >
+          Conéctate con Metamask
+        </MetaMaskButton>
+      ) }
+      { loading && <Loader/> }
+    </div>
   );
 }
 
